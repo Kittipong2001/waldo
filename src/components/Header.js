c986276711630043a19ff1ca0baeb0837d29e895
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-06-15 15:42:22 +0700

    Finish header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+	return (
+		<div className="header">
+			<h1>Waldo</h1>
+			<div className="utilities">
+				<h3>00:00:00</h3>
+				<button>Back To Menu</button>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
